fbdd68e8991793b0854b26de8a8db404dbd1a8b0
Eric Kreis <erickreis793@gmail.com>
2021-04-22 12:16:06 -0300

    Primeiro Dia

---
+++ dia1/primitiveTypes.js
@@ -0,0 +1,32 @@
+/* let movie = "Avangers";  // string literal
+let score = 10.89;  // number literal
+let isValid = true;  // boolean
+let name;  // undefined
+let color = null  // null
+*/
+
+// +, -, *, /, **;
+
+/*
+let salary = 3500;
+salary--  //++
+console.log(salary);
+*/
+
+/*
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: "Ana",
+  lastName: "Santos",
+};
+const patientEmail = "ana@trybe.com"
+
+console.log(typeof patientEmail)
+*/
+
+let base = 5;
+let altura = 8;
+let area = base * altura;
+let perimetro = (2 * base) + (2 * altura);
+console.log(area + perimetro);
---
+++ dia1/var&const.js
@@ -0,0 +1,8 @@
+const name = "Eric"
+const birthCity = "Brasília"
+let birthYear =  2002
+birthYear = 2030
+/*birthCity = "Natal" -> It isn't work because "birthCity is a constant"*/
+console.log(name)
+console.log(birthCity)
+console.log(birthYear)
