c43a1bc822945ab462792782d40d8579b0072366
Eric Kreis <erickreis793@gmail.com>
2021-04-23 14:53:55 -0300

    Add 'dia2' with exBeforeClass

---
+++ dia2/exBeforeClass/ex004.js
@@ -0,0 +1,4 @@
+let numero = 8;
+for (let index = 1; index <=10; index++) {
+  console.log(`${numero} X ${index} = ${numero * index}`)
+}
---
+++ dia2/exBeforeClass/ex006.js
@@ -0,0 +1,6 @@
+// for of
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let name of names) {
+  name += ' Alfinito';
+  console.log(name);
+}
