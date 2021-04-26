bffbabea25e094d581f34608314655e0c561e91a
Eric Kreis <erickreis793@gmail.com>
2021-04-26 18:57:38 -0300

    Resolver exercicio 5

--- dia3/exAfterClass/ex005.js
+++ dia3/exAfterClass/ex005.js
@@ -1,19 +1,19 @@
 const n = 5;
 let asterisco = '*';
-let line = '';
 let mid = (n + 1) / 2;
+let time = 1;
 let left = mid;
 let right = mid;
-for (let linha = 0; linha <= mid; linha ++) {
-  for(let column = 0; column <= n; column ++) {
-    if (column > left && column < right) {
+for (let linha = 1; linha <= mid; linha ++) {
+  let line = '';
+  for (let column = 1; column <= n; column ++) {
+    if (column == left || column == right || linha == mid){
       line += asterisco;
     } else {
       line += ' ';
     };
   };
   console.log(line);
-  line = '';
   left--;
   right++;
 };
