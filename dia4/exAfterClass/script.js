0d61d389b415de0a4f2e63c654b8c625528ec8af
Eric Kreis <erickreis793@gmail.com>
2021-05-04 20:38:25 -0300

    adiciona exAfterClass

---
+++ dia4/exAfterClass/script.js
@@ -0,0 +1,59 @@
+window.onload = function () {
+  function createColorButton(ID) {
+    const colorButton = document.createElement('button');
+    const sectionOfButtons = document.getElementById('color-buttons');
+    colorButton.className = 'button';
+    colorButton.id = ID;
+    sectionOfButtons.appendChild(colorButton);
+  }
+
+  createColorButton('white')
+  createColorButton('blue');
+  createColorButton('purple');
+  createColorButton('pink');
+  createColorButton('green');
+
+  function colorButtonsExec () {
+    const whiteButton = document.getElementById('white');
+    const blueButton = document.getElementById('blue');
+    const purpleButton = document.getElementById('purple');
+    const pinkButton = document.getElementById('pink');
+    const greenButton = document.getElementById('green');
+    let corpo = document.querySelectorAll('body');
+
+    whiteButton.addEventListener('click', function () {
+      corpo[0].style.backgroundColor = 'white';
+    });
+    blueButton.addEventListener('click', function () {
+      corpo[0].style.backgroundColor = '#B9D7EA';
+    });
+    purpleButton.addEventListener('click', function () {
+      corpo[0].style.backgroundColor = '#CEBEE1';
+    });
+    pinkButton.addEventListener('click', function () {
+      corpo[0].style.backgroundColor = '#F7DBD7';
+    });
+    greenButton.addEventListener('click', function () {
+      corpo[0].style.backgroundColor = '#C7F7C8';
+    });
+
+
+    whiteButton.addEventListener('mouseover', function () {
+      whiteButton.style.cursor = 'pointer'
+    });
+    blueButton.addEventListener('mouseover', function () {
+      blueButton.style.cursor = 'pointer'
+    });
+    purpleButton.addEventListener('mouseover', function () {
+      purpleButton.style.cursor = 'pointer'
+    });
+    pinkButton.addEventListener('mouseover', function () {
+      pinkButton.style.cursor = 'pointer'
+    });
+    greenButton.addEventListener('mouseover', function () {
+      greenButton.style.cursor = 'pointer'
+    });
+  }
+  
+  colorButtonsExec();
+}
