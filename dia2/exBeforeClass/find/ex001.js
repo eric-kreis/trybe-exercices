6a24f747a75e35aaa5dd7e02279736a6379deaf3
Eric Kreis <erickreis793@gmail.com>
2021-05-26 16:57:46 -0300

    cria dia2 com todos exBeforeClass

---
+++ dia2/exBeforeClass/find/ex001.js
@@ -0,0 +1,9 @@
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  return numbers.find(number =>
+    (number % 5 === 0 && number % 3 === 0)
+  );
+};
+
+console.log(findDivisibleBy3And5());
