3e46591affcc57821ae3ce70b94b9975c9b7e5ea
Eric Kreis <erickreis793@gmail.com>
2021-05-28 17:22:09 -0300

    Adiciona dia4 com exBeforeCLass

---
+++ dia4/exBeforeClass/ex001.js
@@ -0,0 +1,113 @@
+const assert = require('assert');
+
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 },
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
+
+const expected = [
+  { name: 'Jorge', materia: 'Português' },
+  { name: 'Mario', materia: 'Biologia' },
+  { name: 'Jorge', materia: 'Português' },
+  { name: 'Maria', materia: 'Química' },
+  { name: 'Natalia', materia: 'Matemática' },
+  { name: 'Wilson', materia: 'Português' },
+];
+
+const useReduce = () => {
+  const higherScore = (subject1, subject2) => (
+    subject1.nota >= subject2.nota ? subject1 : subject2
+  ); 
+  const bestSubjectPerStudent = estudantes.map(student => {
+    return {
+      name: student.nome,
+      materia: student.materias.reduce(higherScore).name,
+    }
+  });
+  return bestSubjectPerStudent;
+}
+
+console.log(useReduce());
+
+/* const getBestClass = (acc, materia) => {
+  if (acc.nota >= materia.nota) return acc;
+  return materia;
+};
+
+const reportBetter = (students) => students.map((student) => ({
+  name: student.nome,
+  materia: student.materias.reduce(getBestClass).name}));
+
+console.log(reportBetter(estudantes)); */
+
+assert.deepStrictEqual(useReduce(), expected);
