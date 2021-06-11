e4cf6a2264b89e3d36c4eb112cee502b4711855d
Eric Kreis <erickreis793@gmail.com>
2021-06-11 06:51:49 -0300

    Ex002 da secondPart

---
+++ dia1/exAfterClass/secondPart/ex002.js
@@ -0,0 +1,8 @@
+function techList(skills, name) {
+  if (skills.length === 0) { return 'Vazio!'; }
+  skills.sort();
+  return skills.map((skill) =>
+    ({ tech: skill, name, }));
+}
+
+module.exports = techList;
