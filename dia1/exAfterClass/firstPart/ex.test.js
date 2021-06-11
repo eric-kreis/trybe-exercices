d5dfdfd6ca13facb700e1b9b7d200cdb2d784f13
Eric Kreis <erickreis793@gmail.com>
2021-06-11 03:41:12 -0300

    firstPart with ex002.js

--- dia1/exAfterClass/firstPart/ex.test.js
+++ dia1/exAfterClass/firstPart/ex.test.js
@@ -1,6 +1,5 @@
-const sum = require('./ex001');
-
-describe('exercicio 01', () => {
+describe('Exercício 01', () => {
+  const sum = require('./ex001');
   it('Test if sum has error when "typeof" a parameter is not a number', () => {
     expect(() => { sum('1', 2) }).toThrow();
   });
@@ -12,5 +11,15 @@ describe('exercicio 01', () => {
   });
   it('Test if the sum is returning a correct number', () => {
     expect(sum(0, 0)).toBe(0);
+  });
+});
+
+describe('Exercício 02', () => {
+  const myRemove = require('./ex002');
+  it('Test if "myRemove" returns the expected Array', () => {
+    expect([1, 2, 4]).toEqual(myRemove([1, 2, 3, 4], 3));
+  });
+  it('Test if "myRemove" not returns the unexpected Array', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
   });
 });
