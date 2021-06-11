2824fa2011274bdec2244d091b16ac6ae76e8ceb
Eric Kreis <erickreis793@gmail.com>
2021-06-11 06:52:30 -0300

    Refatora ex002

--- dia1/exAfterClass/secondPart/ex002.js
+++ dia1/exAfterClass/secondPart/ex002.js
@@ -1,8 +1,7 @@
 function techList(skills, name) {
   if (skills.length === 0) { return 'Vazio!'; }
   skills.sort();
-  return skills.map((skill) =>
-    ({ tech: skill, name, }));
+  return skills.map((skill) => ({ tech: skill, name, }));
 }
 
 module.exports = techList;
