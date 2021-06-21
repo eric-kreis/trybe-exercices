017aa34b0bf43c3f889e7159f451b51eed13bb64
Eric Kreis <erickreis793@gmail.com>
2021-06-21 05:50:54 -0300

    resolvendo conflito

--- dia1/exAfterClass/secondPart/exercices/ex002.js
+++
@@ -1,8 +0,0 @@
-function techList(skills, name) {
-  if (skills.length === 0) { return 'Vazio!'; }
-
-  skills.sort();
-  return skills.map((skill) => ({ tech: skill, name, }));
-}
-
-module.exports = techList;
--- dia2/exBeforeClass/callBack/ex001.test.js
+++
@@ -1,8 +0,0 @@
-// Callback done() do jest.
-
-test("Só passa de verdade com o done!", (done) => {
-  setTimeout(() => {
-    expect(10).toBe(10);
-    done();
-  }, 500);
-});
