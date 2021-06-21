eb5ae770d4fda6dee45a11720e8ae83f911faed1
Eric Kreis <erickreis793@gmail.com>
2021-06-21 04:27:31 -0300

    dia1 com exBefore class

---
+++ dia1/exBeforeClass/tofix/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import WhoAmI from './Component';
+
+function App() {
+  return (<WhoAmI/>);
+}
+
+export default App;
---
+++ dia1/exBeforeClass/tofix/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class WhoAmI extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Eric Kreis</h1>
+        <p>Me chamo Eric, tenho 19 anos e sou de Brasília/DF.</p>
+      </header>
+    );
+  }
+}
+
+export default WhoAmI;
