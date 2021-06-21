4df4c9ea10a00889da0a0480baedf0392fab83bc
Eric Kreis <erickreis793@gmail.com>
2021-06-21 20:34:07 -0300

    test with the shoppingListArray

--- dia2/exBeforeClass/components-array/src/App.js
+++ dia2/exBeforeClass/components-array/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import ShoppingList from './components/ShppingList';
 
 function App() {
-  // return ();
+  return (
+    <div>
+      <h2>Shopping List</h2>
+      <ul>
+      { ShoppingList }
+      </ul>
+    </div>
+  );
 }
 
 export default App;
---
+++ dia2/exBeforeClass/components-array/src/components/ShppingList.js
@@ -0,0 +1,8 @@
+const list = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
+
+const ShoppingList = list.map((item, index) => {
+  console.log("item: ", item);
+  return (<li key={ index }>{ item }</li>);
+});
+
+export default ShoppingList
