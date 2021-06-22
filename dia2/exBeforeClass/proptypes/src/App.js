378b9e31822c335e305bc3577ce9d00e59da81a1
Eric Kreis <erickreis793@gmail.com>
2021-06-21 22:11:22 -0300

    cria propTypes

--- dia2/exBeforeClass/proptypes/src/App.js
+++ dia2/exBeforeClass/proptypes/src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
+import Greeting from './components/Greeting';
 
 function App() {
-  // return (
-    
-  // );
+  return ( <Greeting name="Eric" lastName="Kreis"/> );
 }
 
 export default App;
