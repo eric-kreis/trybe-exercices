8e4b9fb5f61d6d1318c5c0ab3b4793e8b02333e0
Eric Kreis <erickreis793@gmail.com>
2021-06-23 03:55:11 -0300

    last changes

--- dia2/exAfterClass/my-pokedex/src/components/Pokedex.js
+++ dia2/exAfterClass/my-pokedex/src/components/Pokedex.js
@@ -2,8 +2,8 @@ import Pokemon from "./Pokemon";
 import pokemons from "../data";
 
 function PokeDex() {
-  return pokemons
-    .map((pokemon) => <Pokemon key={ pokemon.id } pokemon={ pokemon }/>);
+  return pokemons.map((pokemon) => 
+    <Pokemon key={ pokemon.id } pokemon={ pokemon }/>);
 }
 
 export default PokeDex;
