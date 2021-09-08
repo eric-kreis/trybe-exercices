046b0bbced820d331325f0ed2dd5fb4bc51c5c5e
Eric Kreis <erickreis793@gmail.com>
2021-09-08 19:43:06 -0300

    Dia 1 com os exercios pós aula

---
+++ dia1/exAfterClass.sql
@@ -0,0 +1,11 @@
+SELECT * FROM sakila.city;
+/*city to country "N:1"*/
+
+SELECT * FROM sakila.address;
+/* address to city "N:1" */
+
+SELECT * FROM sakila.film_category;
+/* film to category "N:1" */
+
+SELECT * FROM sakila.inventory;
+/* film to inventory "1:N" */
