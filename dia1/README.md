1a621a382d21cdc082df7efa548a843124e38570
Eric Kreis <erickreis793@gmail.com>
2021-09-08 17:26:56 -0300

    Iniciando back-end :rocket:

--- dia1/README.md
+++
@@ -1,3 +0,0 @@
-Today I did here exercices to learn pure Redux.
-
-Tomorrow I'm going to learn react-redux :).
--- dia2/ex-before-class/src/data/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux';
-import rootReducer from '../reducers/index';
-
-const store = createStore(rootReducer,
-window.__REDUX__DEVTOOLS__EXTENSION__ && window.__REDUX__DEVTOOLS__EXTENSION__());
-
-export default store;
